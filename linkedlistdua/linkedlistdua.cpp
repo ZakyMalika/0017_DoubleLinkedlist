b0639dd5c2dd5f19a2c1a1c79bd01509f7396562
ZakyMalika <zakymalika0987@gmail.com>
2024-05-04 12:01:46 +0700

    membuat step 2 memasukkan new node

--- linkedlistdua/linkedlistdua.cpp
+++ linkedlistdua/linkedlistdua.cpp
@@ -16,4 +16,12 @@ void addNode()
 	Node* newNode = new Node();		//step 1: create a new code
 	cout << "\nEnter the roll number of the student: ";
 	cin >> newNode->noMhs; // Assign value to the data field of the new Node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // Assign value to the data field of the new node
+
+	// insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{ //step 2: insert the new node at the beginning
+
+	}
 }
