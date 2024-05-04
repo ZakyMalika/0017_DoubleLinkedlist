fb5353ea8efaa67394fd5940cf94444a7873c041
ZakyMalika <zakymalika0987@gmail.com>
2024-05-04 12:31:04 +0700

    membuat ouput yang ingin di delete

--- linkedlistdua/linkedlistdua.cpp
+++ linkedlistdua/linkedlistdua.cpp
@@ -82,5 +82,9 @@ bool search(int rollNo, Node** previous, Node** current)
 
 void deletionNode()
 {
+	Node* previous, * current;
+	int rollNo;
 
+	cout << "\nEnter the roll number of the student whose record is the to be deleted: ";
+	cin >> rollNo; //step 3: get the roll number to be delete
 }
