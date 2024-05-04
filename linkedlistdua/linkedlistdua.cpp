b199cde988d6787887643a7228a893830d27a072
ZakyMalika <zakymalika0987@gmail.com>
2024-05-04 12:46:35 +0700

    membuat rangkaian dari fungsi listempty

--- linkedlistdua/linkedlistdua.cpp
+++ linkedlistdua/linkedlistdua.cpp
@@ -139,5 +139,19 @@ bool ListEmpty()
 
 void tranverse()
 {
-	
+	if (ListEmpty())
+		cout << "\nList is Empty" << endl;
+	else
+	{
+		cout << "\nRecord in discending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL);
+		currentNode = currentNode->next;
+
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
 }
